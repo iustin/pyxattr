b18079e8926251fe11010f68a77abedbb6e66b3b
Iustin Pop <iusty@k1024.org>
2002-12-21 21:20:12 +0000

    Typos

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ from distutils.core import setup, Extension
 
 setup(name="pyxattr",
       version="0.1",
+      description="Extended attributes for python",
+      long_description="""This is a C extension module for Python which
+      implements extended attributes manipulation. It is a wrapper on top
+      of the attr C library - see attr(5).""",
+      author="Iustin Pop",
+      author_email="iusty@k1024.org",
       ext_modules=[Extension("xattr", ["xattr.c"], libraries=["attr"])],
       )
