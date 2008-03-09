2408a4aa1a277625ae6b9910d8deb970a5292e4e
Iustin Pop <iusty@k1024.org>
2008-03-09 11:15:13 +0100

    Switch from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import distutils
-from distutils.core import setup, Extension
+#import distutils
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 long_desc = """This is a C extension module for Python which
 implements extended attributes manipulation. It is a wrapper on top
 of the attr C library - see attr(5)."""
 version = "0.2.2"
 
-setup(name="pyxattr",
-      version=version,
-      description="Extended attributes for python",
-      long_description=long_desc,
-      author="Iustin Pop",
-      author_email="iusty@k1024.org",
-      url="http://pyxattr.sourceforge.net",
-      license="GPL",
-      ext_modules=[Extension("xattr", ["xattr.c"], libraries=["attr"])],
+setup(name = "pyxattr",
+      version = version,
+      description = "Filesystem extended attributes for python",
+      long_description = long_desc,
+      author = "Iustin Pop",
+      author_email = "iusty@k1024.org",
+      url = "http://pyxattr.sourceforge.net",
+      license = "LGPL",
+      ext_modules = [Extension("xattr", ["xattr.c"], libraries=["attr"])],
+      test_suite = "test/test_xattr",
       )
