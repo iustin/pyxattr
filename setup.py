83ee6aaa06f90b14962eb93558e7e8711593b067
Iustin Pop <iusty@k1024.org>
2008-06-30 09:08:45 +0200

    Docstring updates
    More epydoc features are now used for prettier output.

--- setup.py
+++ setup.py
@@ -8,15 +8,22 @@ long_desc = """This is a C extension module for Python which
 implements extended attributes manipulation. It is a wrapper on top
 of the attr C library - see attr(5)."""
 version = "0.3.0"
-
+author = "Iustin Pop"
+author_email = "iusty@k1024.org"
+macros = [
+    ("_XATTR_VERSION", '"%s"' % version),
+    ("_XATTR_AUTHOR", '"%s"' % author),
+    ]
 setup(name = "pyxattr",
       version = version,
       description = "Filesystem extended attributes for python",
       long_description = long_desc,
-      author = "Iustin Pop",
-      author_email = "iusty@k1024.org",
+      author = author,
+      author_email = author_email,
       url = "http://pyxattr.sourceforge.net",
       license = "LGPL",
-      ext_modules = [Extension("xattr", ["xattr.c"], libraries=["attr"])],
+      ext_modules = [Extension("xattr", ["xattr.c"],
+                               libraries=["attr"],
+                               define_macros=macros)],
       test_suite = "test/test_xattr",
       )
