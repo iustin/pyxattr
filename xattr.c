iustin/pyxattr

commit 433c58520ad549e79f8233abd9793536d7d6f48d

Implement changes needed for PEP 353 compliance
It seems the coded wrongly mixed ssize_t and ints in a couple of places,
plus not properly using Py_ssize_t as needed. Fix these and ensure
backwards compat with python 2.4.

xattr.c
@@ -1,5 +1,13 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <attr/xattr.h>
+
+/* Compatibility with python 2.4 regarding python size type (PEP 353) */
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
 
 typedef enum {T_FD, T_PATH, T_LINK} target_e;
 
@@ -45,7 +53,7 @@ static ssize_t _get_obj(target_t *tgt, char *name, void *value, size_t size) {
         return getxattr(tgt->name, name, value, size);
 }
 
-static ssize_t _set_obj(target_t *tgt, char *name, void *value, size_t size,
+static int _set_obj(target_t *tgt, char *name, void *value, size_t size,
                         int flags) {
     if(tgt->type == T_FD)
         return fsetxattr(tgt->fd, name, value, size, flags);
@@ -55,7 +63,7 @@ static ssize_t _set_obj(target_t *tgt, char *name, void *value, size_t size,
         return setxattr(tgt->name, name, value, size, flags);
 }
 
-static ssize_t _remove_obj(target_t *tgt, char *name) {
+static int _remove_obj(target_t *tgt, char *name) {
     if(tgt->type == T_FD)
         return fremovexattr(tgt->fd, name);
     else if (tgt->type == T_LINK)
@@ -88,7 +96,7 @@ pygetxattr(PyObject *self, PyObject *args)
     int nofollow=0;
     char *attrname;
     char *buf;
-    int nalloc, nret;
+    ssize_t nalloc, nret;
     PyObject *res;
 
     /* Parse the arguments */
@@ -159,7 +167,8 @@ pysetxattr(PyObject *self, PyObject *args)
     int nofollow=0;
     char *attrname;
     char *buf;
-    int bufsize, nret;
+    Py_ssize_t bufsize;
+    int nret;
     int flags = 0;
     target_t tgt;
 
@@ -239,10 +248,10 @@ pylistxattr(PyObject *self, PyObject *args)
 {
     char *buf;
     int nofollow=0;
-    int nalloc, nret;
+    ssize_t nalloc, nret;
     PyObject *myarg;
     PyObject *mylist;
-    int nattrs;
+    Py_ssize_t nattrs;
     char *s;
     target_t tgt;
 

commit a37fdf32f3acc611af95410d25005e7bfb645454

Fix string/unicode buffer size parsing
It seems that CPython allows passing "wrong" pointers for et#
(Py_ssize_t instead of int), but PyPy has problems with this, and
wrongly casts the pointers, leading to unsigned/signed conversion
failures.

This bug was introduced in commits 433c5852 and 8aa6fd81; I'm no
longer sure why bufsize was changed from int to Py_ssize_t as well,
but passing an int value to a C function expecting an ssize_t should
be safe as the compiler should do the required conversions behind the
scenes (as long as it has correct information about the types).

Closes gh-1.

xattr.c
@@ -1,7 +1,7 @@
 /*
     xattr - a python module for manipulating filesystem extended attributes
 
-    Copyright (C) 2002, 2003, 2006, 2008, 2012 Iustin Pop <iusty@k1024.org>
+    Copyright (C) 2002, 2003, 2006, 2008, 2012, 2013 Iustin Pop <iusty@k1024.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -593,7 +593,7 @@ pysetxattr(PyObject *self, PyObject *args)
     int nofollow = 0;
     char *attrname = NULL;
     char *buf = NULL;
-    Py_ssize_t bufsize;
+    int bufsize;
     int nret;
     int flags = 0;
     target_t tgt;
@@ -659,7 +659,7 @@ xattr_set(PyObject *self, PyObject *args, PyObject *keywds)
     int nofollow = 0;
     char *attrname = NULL;
     char *buf = NULL;
-    Py_ssize_t bufsize;
+    int bufsize;
     int nret;
     int flags = 0;
     target_t tgt;