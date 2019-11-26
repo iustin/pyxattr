cb0c926f388d6df073091936cd0a115ffb763d61
Iustin Pop <iustin@k1024.org>
2019-11-26 04:29:12 +0100

    Bump version numbers for new release
    No sense in waiting more, this is good conversion to Python 3 +
    feature request + a bug fix.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 long_desc = """This is a C extension module for Python which
 implements extended attributes manipulation. It is a wrapper on top
 of the attr C library - see attr(5)."""
-version = "0.6.1"
+version = "0.7.0"
 author = "Iustin Pop"
 author_email = "iustin@k1024.org"
 libraries = []
